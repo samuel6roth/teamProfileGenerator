eb1441258cbf60607a390c3dfeb618c10f91c28a
Samuel Roth <samuel6roth@gmail.com>
2021-04-13 00:02:52 -0400

    Setting up prompt questions in index.js file

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+function initIndex() {
+    addMember();
+}
+
+function addMember() {
+    inquirer.prompt([{
+        message: 'Please enter team members name',
+        name: 'name'
+    },
+    {
+        type: 'list',
+        message: 'Select team members role',
+        choices: [
+            'Engineer',
+            'Intern',
+            'Manager'
+        ],
+        name: 'role'
+    },
+    {
+        message: 'Please enter team members ID',
+        name: 'id'
+    },
+    {
+        message: 'Enter team members email address',
+        name: 'email'
+    }
+    ])
+}
+
+initIndex();
